26967df70c706ffa1e24d3c99eec0b55c56f915e
J-HaleOf76 <johnhale1976@gmail.com>
2022-02-26 17:24:50 -0500

    finishing celsius to fahrenheit conversion

--- class13-materials/class13-materials/temperature-converter/js/main.js
+++ class13-materials/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,20 @@
-//Write your pseduo code first! 
+//Write your pseudo code first! 
+
+// take in user values celsius
+// convert to fahrenheit
+// display results
+// 0 to 32 / what's the formula for the conversion
+//
+
+document.querySelector('#check').addEventListener('click', convert)
+
+function convert() {
+    let temp = document.querySelector('#Celsius').value
+
+    temp = temp * 9/5 + 32
+
+    document.querySelector('#placeToYell').innerText = temp
+};
+// Got it working OK
+// Now make this into an web application that I want to use for myself
+// The what and why of celsius and fahrenheit conversions.
