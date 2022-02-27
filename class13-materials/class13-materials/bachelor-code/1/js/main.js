9f4290e2d33fbaf8e1382b018323e44b8025a09d
J-HaleOf76 <johnhale1976@gmail.com>
2022-02-26 23:50:11 -0500

    finished bachelor

--- class13-materials/class13-materials/bachelor-code/1/js/main.js
+++ class13-materials/class13-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,8 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#finalRose').addEventListener('click', hide)
 
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	document.querySelector("#claire").style.display = 'none'
+	document.querySelector("#sharleen").style.display = 'none'
 }
+
+// I've made 2 of the 3 disappear
