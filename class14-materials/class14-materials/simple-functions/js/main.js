4031fadcdd8f6658656212c2991ddd4eef92fe8b
J-HaleOf76 <johnhale1976@gmail.com>
2022-03-06 00:44:51 -0500

    class16

--- class14-materials/class14-materials/simple-functions/js/main.js
+++ class14-materials/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,23 @@
 //create a function that adds two numbers and alerts the sum
 
-//create a function that multiplys three numbers and console logs the product
+
+  // function addtwo(){
+  //   let num1 = 7;
+  //   let num2 = 5;
+  // let sum = num1 + num2;
+  //
+  // alert(sum)
+  // }
+// addtwo();
+// I double checked this online / It's Correct
+// Be careful with strings and numbers
+
+//create a function that multiples three numbers and console logs the product
+function multi3() {
+ "num1" * "num2" * "num3" = sum;
+  
+}
+
+console.log(sum)
 
 //create a function that divides two numbers and returns the ???
---
+++ class_notes/main-class-note.md
@@ -0,0 +1,16 @@
+# main note for the class
+
+- Hosting netlify.com
+
+- Use templates whenever you can  HTML
+
+- - - -
+
+TODO:
+check the sites - social sites, networking, my social media, market places, LinkedIn - Jobs sites - local Business, CraigsList, etc..
+
+- spreadsheet of who to contact
+- phone numbers names addresses - the results of each attempt.
+  
+- - - -
+
