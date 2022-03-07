241795ce0d939151680bc3369e3fd2d6505f45a0
J-HaleOf76 <johnhale1976@gmail.com>
2022-03-07 00:14:22 -0500

    adding class16

--- class14-materials/class14-materials/simple-functions/js/main.js
+++ class14-materials/class14-materials/simple-functions/js/main.js
@@ -10,14 +10,29 @@
   // }
 // addtwo();
 // I double checked this online / It's Correct
-// Be careful with strings and numbers
+// Be careful with strings and integers
+// This is what was given as the finished example
+function addTwoNumbers(num1, num2) {
+  const sum = num1 + num2
+  alert(sum)
+}
+addTwoNumbers(5,7)
+// So this looks like what I started to do
+// I kinda had this right in the beginning. When calling my function I just needed to give it the variables
+
 
 //create a function that multiples three numbers and console logs the product
-function multi3() {
- "num1" * "num2" * "num3" = sum;
+function multi3(num1,num2,num3) {
+ const sum = "num1" * "num2" * "num3"
+ console.log(sum)
   
 }
+multi3(7,4,9)
 
-console.log(sum)
 
 //create a function that divides two numbers and returns the ???
+function divide2(num1,num2) {
+
+  return num1 / num2
+}
+console.log(divide2)
