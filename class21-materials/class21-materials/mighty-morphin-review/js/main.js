97719b62263a35cef15b6c324a445bbcb0b317f8
J-HaleOf76 <johnhale1976@gmail.com>
2022-03-31 16:05:52 -0400

    adding upto CLASS22 unfinished

--- class21-materials/class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/class21-materials/mighty-morphin-review/js/main.js
@@ -1,11 +1,23 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+let holliday = 'Christmax';
+holliday = holliday.toUpperCase()
+console.log(holliday);
+
 
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let myName = "JohnHale"
+myName = myName.toString()
+console.log(myName[-1,-2,-3]);
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-
+function fiveNumsSub(n1,n2,n3,n4,n5) {
+  let sum = (n1+n2+n3+n4+n5 - 100).Math.absolute()
+  // alert(sum.Math.absolute())
+  console.log(sum.Math.absolute());
+}
+fiveNumsSub(3,4,5,6,7)
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
 
